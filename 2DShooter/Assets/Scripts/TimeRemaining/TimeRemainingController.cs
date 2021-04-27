fdaf1feafa981b4913dc3036c885a5e1d7d5b4ae
artyom <goastgavrish444@gmail.com>
2021-04-27 13:16:36 +0300

    Task 2 Finished

--- 2DShooter/Assets/Scripts/TimeRemaining/TimeRemainingController.cs
+++ 2DShooter/Assets/Scripts/TimeRemaining/TimeRemainingController.cs
@@ -1,7 +1,50 @@
-﻿namespace Shooter
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shooter
 {
-    public class TimeRemainingController
+    public sealed class TimeRemainingController
     {
+        #region Fields
+
+        private readonly List<ITimeRemaining> _timeRemainings;
+
+        #endregion
+
+        #region ClassLifeCycles
+
+        public TimeRemainingController()
+        {
+            _timeRemainings = TimeRemainingExtensions.TimeRemainings;
+        }
+
+        #endregion
+
+        #region MyRegion
+
+        public void Execute()
+        {
+            var time = Time.deltaTime;
+            for (int i = 0; i < _timeRemainings.Count; i++)
+            {
+                var obj = _timeRemainings[i];
+                obj.CurrentTime -= time;
+                if (obj.CurrentTime <= 0.0f)
+                {
+                    obj.Method.Invoke();
+                    if (!obj.IsRepeating)
+                    {
+                        obj.RemoveTimeRemaining();
+                    }
+                    else
+                    {
+                        obj.CurrentTime = obj.Time;
+                    }
+                }
+            }
+        }
+
+        #endregion
         
     }
 }
