dfe8851dd9c6e9fd85f3bdb1ac16b75f50cc7e10
artyom <goastgavrish444@gmail.com>
2021-04-27 16:00:27 +0300

    Task 3 in progress

---
+++ 2DShooter/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shooter
+{
+    public sealed class CameraShake : MonoBehaviour
+    {
+        public Animator camShake;
+
+        public void Shake()
+        {
+            camShake.SetTrigger("shake");
+        }
+    }
+}
+
