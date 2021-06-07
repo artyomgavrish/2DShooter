373dd3e73365de0939c7fdc651eb3c91eb99b7f6
artyom <goastgavrish444@gmail.com>
2021-06-07 22:41:56 +0300

    Lesson 7, Task 1-2 complete

---
+++ 2DShooter/Assets/Editor/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace Geekbrains
+{
+    public class Logger : EditorWindow
+    {
+
+        public static Logger ShowWindow()
+        {
+            Logger window = GetWindow<Logger>();
+            return window;
+        }
+
+        private void OnEnable()
+        {
+            TextField message = new TextField("Сообщение");
+            ColorField color = new ColorField("Выбор цвета");
+
+            Button send = new Button(() =>
+            {
+                Debug.Log(message);
+            });
+            send.text = "Отправить!";
+            
+            rootVisualElement.Add(message);
+            rootVisualElement.Add(color);
+            rootVisualElement.Add(send);
+        }
+    }
+}
---
+++ 2DShooter/Assets/Editor/MenuItems.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+
+namespace Geekbrains
+{
+    public class MenuItems
+    {
+        [MenuItem("Geekbrains/RadiusSpam ")]
+        private static void MenuOption()
+        {
+            EditorWindow.GetWindow(typeof(RadiusSpam), false, "RadiusSpam");
+        }
+        
+        [MenuItem("Geekbrains/Logger ")]
+        private static void MenuOption1()
+        {
+            EditorWindow.GetWindow(typeof(Logger), false, "Logger");
+        }
+    }
+}
